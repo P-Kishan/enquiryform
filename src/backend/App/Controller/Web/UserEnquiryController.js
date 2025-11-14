9f89e0b468548429d2386345df0e81712d5e55e3
Paradava Kishan <p.k.ahir9099@gmail.com>
2025-11-14 21:53:51 +0530

    db error solve

--- src/backend/App/Controller/Web/UserEnquiryController.js
+++ src/backend/App/Controller/Web/UserEnquiryController.js
@@ -39,25 +39,51 @@ let ViewData =  async (req,res)=>{
 
     }
 
-let DeleteData  = async (req,res)=>{
+let DeleteData = async (req, res) => {
+  try {
     let id = req.params.id;
-    let deleteData = await DataModel.deleteOne({_id: id});
-    res.send(id, { delRes: deleteData });
-}
 
-let UpdateData = async (req,res)=>{
+    let deleteData = await DataModel.deleteOne({ _id: id });
+
+    res.status(200).json({
+      success: true,
+      message: "Data Deleted Successfully",
+      deletedId: id,
+      result: deleteData
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Delete Failed",
+      error: err.message
+    });
+  }
+};
+
+let UpdateData = async (req, res) => {
+  try {
     let id = req.params.id;
     let { name, email, phone, message } = req.body;
-    let updateObj = {
-        name: name,
-        email: email,
-        phone: phone,
-        message : message 
-    };
 
-    let updateData = await DataModel.updateOne({_id: id}, updateObj);
-    
-        res.send("Data Updated", updateData);
-    }
+    let updateObj = { name, email, phone, message };
+
+    let updateData = await DataModel.updateOne({ _id: id }, updateObj);
+
+    res.status(200).json({
+      success: true,
+      message: "Data Updated Successfully",
+      updatedId: id,
+      result: updateData
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Update Failed",
+      error: err.message
+    });
+  }
+};
 
 module.exports = {EnquiryInsert, ViewData, DeleteData, UpdateData};
